112ece46e13cc5014794c308d07113b4d1fd343f
samuelvertello <83984319+samuelvertello@users.noreply.github.com>
2021-05-13 21:36:50 -0300

    added

--- Dicas.java
+++ Dicas.java
@@ -4,12 +4,8 @@ public class Dicas {
 
     public void obterDicas(String raca){
 
-        for(int i = 0; i < gerenciar.getPet().size(); i++){
-            if(gerenciar.getPet().get(i).getRaca().equals(raca)){
-                System.out.println("https://www.google.com.br/search?q=dicas"+gerenciar.getPet().get(i).getRaca());
-
-            }
-        }
+            System.out.println("https://www.google.com.br/search?q="+raca);            
+        
 
 
     }
